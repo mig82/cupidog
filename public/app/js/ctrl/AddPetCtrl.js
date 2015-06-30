1f3aa3e4b3f0e20f4cf51ee6a4fb7d080db30e67
Miguelangel <miguelangelxfm@gmail.com>
2015-06-30 17:36:03 +0200

    completed species typeahead, added gmaps autocomplete, added new icons, added flat styles for inputs

--- public/app/js/ctrl/AddPetCtrl.js
+++ public/app/js/ctrl/AddPetCtrl.js
@@ -1,4 +1,4 @@
-angular.module('cupidog').controller('AddPetCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
+angular.module('cupidog').controller('AddPetCtrl', ['$scope', '$http', '$location', '$timeout', '$filter', function($scope, $http, $location, $timeout, $filter){
 
 	"use strict";
 
@@ -7,8 +7,8 @@ angular.module('cupidog').controller('AddPetCtrl', ['$scope', '$http', '$locatio
 	}
 
 	$scope.addPet = function(){
-		console.log("creating new pet %o", $scope.pet);
-
+		$scope.pet.profilePic = "img/icons/nopic_" + $scope.pet.sp + ".svg";
+		$scope.pet.spIco = "#" + $scope.pet.sp + "-ico";
 		$http.post("/pets", $scope.pet).success(function(res){
 			console.log("created %o", res);
 		});
@@ -38,15 +38,43 @@ angular.module('cupidog').controller('AddPetCtrl', ['$scope', '$http', '$locatio
 		"bird",
 		"hamster",
 		"turtle",
+		"tortoise",
 		"fish",
 		"iguana",
 		"huron",
-		"horse"
+		"horse",
+		"chimpanzee",
+		"monkey"
 	];
 
 	$scope.setSpecies = function(sp){
 		$scope.pet.sp = sp;
 		$scope.spSearch = "";
+	};
+
+	$scope.blankSpecies = function(){
+		$scope.pet.sp = "";
+	};
+
+	$scope.resetSearch = function(){
+		
+		var search = $filter('lowercase')($scope.spSearch);
+		var i = $scope.species.indexOf(search);
+		if(i >= 0){
+			$scope.pet.sp = $scope.species[i];
+			$scope.spSearch = "";
+		}
+		else{
+
+			$timeout(function(){
+				if(!$scope.pet.sp){
+					$scope.spSearch = "";
+				}
+			}, 200);
+		}
+
+		
+		
 	};
 
 }]);
